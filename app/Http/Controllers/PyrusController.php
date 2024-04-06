9b3ef52232669982d953f3218d7cf379b4529260
artur <ar2rsoft@gmail.com>
2024-04-06 20:34:37 +0500

    test

--- app/Http/Controllers/PyrusController.php
+++ app/Http/Controllers/PyrusController.php
@@ -121,7 +121,7 @@ class PyrusController extends Controller
         $account_id = $request->post('account_id');
         $access_token = $request->post('access_token');
 
-        Integration::where('id', $account_id)->where('token', $access_token)->update(['active' => $request->post('enabled', false)]);
+        Integration::where('id', $account_id)->where('token', $access_token)->update(['enabled' => $request->post('enabled', false)]);
     }
 
     // POST event
